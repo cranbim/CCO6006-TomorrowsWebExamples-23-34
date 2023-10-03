b9805baf0a4f25b5c1f2b8d0704a64f54d6892a3
Dave Webb <dave.webb@cranbim.com>
2023-10-03 14:15:05 +0100

    Week1 from last year

---
+++ play1/code.js
@@ -0,0 +1,20 @@
+let res=document.getElementById("result");
+res.innerText="Soemthing changed"
+let counter=0;
+let countInterval;
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    countInterval=setInterval(()=>console.log(counter++/10),100);
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    res.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  res.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+  clearInterval(countInterval);
+}
